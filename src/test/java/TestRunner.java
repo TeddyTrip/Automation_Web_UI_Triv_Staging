d0a9da767cdec1dd9319355454d965e400eb6c43
Kimbooo28 <66553277+ClusterID@users.noreply.github.com>
2026-08-07 14:12:53 +0700

    fix-Fix-Search-Asset-By-V-Money-On-Buy-Sell-Swap

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -12,7 +12,7 @@ import org.junit.runner.RunWith;
         "json:target/cucumber-reports/report.json"
     },
     tags = 
-    "@BuyRandomAssetCategory", // Hanya jalankan scenario dengan tag ini
+    "@SwapFlowCSV", // Hanya jalankan scenario dengan tag ini
     monochrome = true
 )
 public class TestRunner {
