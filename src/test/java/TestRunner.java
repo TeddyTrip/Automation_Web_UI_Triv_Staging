d50ab913a42848cdb25aebd76d9270fc942ca6d4
Kimbooo28 <66553277+ClusterID@users.noreply.github.com>
2026-09-21 10:02:20 +0700

    Fix search nama dompet using label parameter from install coin lists

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -12,9 +12,15 @@ import org.junit.runner.RunWith;
         "json:target/cucumber-reports/report.json"
     },
     tags = 
-    "@BuyFlowCSVWithCertainAmount", // Hanya jalankan scenario dengan tag ini
+    "@SwapFlowCSV", // Hanya jalankan scenario dengan tag ini
     monochrome = true
 )
 public class TestRunner {
     // Kosongkan saja, ini hanya sebagai pemicu eksekusi
 }
+
+// @BuyFlowCSVWithCertainAmount
+// @SellFlowCSV
+
+//mvn clean test "-Dcucumber.options=--tags @BuyFlowCSVWithCertainAmount"
+//mvn clean test "-Dcucumber.options=--tags @SellFlowCSV"
--- src/test/java/steps/BuyAssetSteps.java
+++ src/test/java/steps/BuyAssetSteps.java
@@ -25,7 +25,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BuyAssetSteps {
 
-    private WebDriver driver;
+    private WebDriver driver = DriverManager.getDriver();
     private WebDriverWait wait = new WebDriverWait(driver, java.time.Duration.ofSeconds(10));
 
     DashboardPage dashboardPage = new DashboardPage(DriverManager.getDriver(), wait);
